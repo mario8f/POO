225ece3189b35ac20851ef02f3cbee4f23062c29
mario8f <55576317+mario8f@users.noreply.github.com>
2020-04-24 20:40:21 +0200

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+public class Arrays {
+
+
+
+	public static void main(String[] args) {
+
+		
+
+		int [] arr2 = {1, 2, 3, 4, 5};
+
+		System.out.println(arr2.length);
+
+		for (int i = 0; i < (arr2.length-1); i++) {
+
+			System.out.println(arr2[i]);
+
+		}
+
+	}
+
+
+
+}
